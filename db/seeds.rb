552be64cfed0bbe623267105be1cc073fb83e0b2
Charlie Jeppsson <charlie.jeppsson1@gmail.com>
2017-08-26 16:32:16 +0200

    complemented seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,26 +3,42 @@ john = User.create!(email: 'john@beatles.com', password: '12345678', avatar_url:
 paul = User.create!(email: 'paul@beatles.com', password: '12345678', avatar_url: 'http://beatlesthe.free.fr/img/paul.jpg')
 ringo = User.create!(email: 'ringo@beatles.com', password: '12345678', avatar_url: '')
 george = User.create!(email: 'george@beatles.com', password: '12345678', avatar_url: '')
+charlie = User.create!(email: 'charlie.jeppsson1@gmail.com', password: '12345678', avatar_url: 'avatar-charlie.jpg')
 
 puts 'Creating products...'
-startup_stash = Product.create!(
-  name: 'Startup Stash',
-  tagline: 'A curated directory of 400 resources & tools for startups',
-  url: 'http://startupstash.com',
-  user: john
+inline_app = Product.create!(
+  name: 'InLine',
+  tagline: 'Never wait in line again',
+  url: 'http://inline-app.com',
+  user: charlie
 )
 
-startup_launch_list = Product.create!(
-  name: 'Startup Launch List',
-  tagline: 'Articles you need to read before launching a startup',
-  url: 'http://startuplaunchlist.com',
-  user: paul
+tunebook_app = Product.create!(
+  name: 'Tunebook',
+  tagline: 'Your one-stop-shop for discovering new music',
+  url: 'http://tunebook.herokuapp.com',
+  user: charlie
 )
+
+unify_app = Product.create!(
+  name: 'Unify',
+  tagline: "Don't miss out on valuable connections",
+  url: 'http://uni-fy.eu',
+  user: charlie
+)
+
 
 puts 'Creating votes...'
-john.up_votes startup_stash
-paul.up_votes startup_stash
-george.up_votes startup_stash
+john.up_votes inline_app
+paul.up_votes inline_app
+george.up_votes inline_app
+charlie.up_votes inline_app
 
-ringo.up_votes startup_launch_list
+paul.up_votes unify_app
+george.up_votes unify_app
+charlie.upvotes unify_app
+
+charlie.upvotes tunebook_app
+ringo.up_votes tunebook_app
+
 puts 'Finished!'
