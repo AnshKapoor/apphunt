927038e449dbfa8d445fa919d2d63296a97a7533
Charlie Jeppsson <charlie.jeppsson1@gmail.com>
2017-09-05 01:36:41 +0200

    get CommentList component to create new comment (not asynchronously)

--- app/assets/javascripts/components/_comment_list.js.jsx
+++ app/assets/javascripts/components/_comment_list.js.jsx
@@ -6,18 +6,18 @@ var CommentList = React.createClass({
     }
   },
 
-  postComment: function() {
-    // var content = React.findDOMNode(this.refs.commentInput).value.trim();
-    // console.log(content);
+  postComment(event) {
+    // var test_content = React.findDOMNode(this.refs.commentInput).value.trim();
+    event.preventDefault();
+    var test_content = this.refs.content.value;
 
     // Store this (the upvote component) in a variable so that it can be called upon
     // success in the ajax callback. Else this will refer to the AJAX call itself.
     var that = this;
-    var test_content = "Test";
+
     // AJAX request to store the comment
     $.ajax({
       type: 'POST',
-      // url: Routes.post_comment_product_path(this.props.product.id)+"?content=Test comment",
       url: Routes.post_comment_product_path({
         product_id: this.props.product.id,
         content: test_content
@@ -41,14 +41,21 @@ var CommentList = React.createClass({
         })}
         <div>
 
+          <form acceptCharset="UTF-8" className="comment-form">
+            <div className="form-group">
+              <input ref="content" name="content" type="text" placeholder="Write new comment here" className="comment-input form-control"/>
+              <input className="btn btn-success" onClick={this.postComment} type="submit" value="submit" />
+            </div>
+          </form>
 
-          <div onClick={this.postComment}>
-            <div className="product-arrow"></div>
-          </div>
         </div>
       </div>
     );
   },
 });
+
+// <div onClick={this.postComment}>
+//   <div className="product-arrow"></div>
+// </div>
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -7,23 +7,14 @@ Rails.application.routes.draw do
   resources :products do
     member do
       post :upvote  # /products/:id/upvote
-      # post :post_comment # /products/:id/post_comment
     end
   end
 
-  post 'products(/:product_id)(/post_comment/:content)', controller: 'products', action: 'post_comment', as: 'post_comment_product'
-
-  get 'comments', to: 'comments#index'
-  # get 'post_comment', to: 'comments#create'
+  post 'products/:product_id/post_comment/:content', controller: 'products', action: 'post_comment', as: 'post_comment_product'
 
   # get 'for_sale', to: 'products#for_sale'
   # get 'wanted', to: 'products#wanted'
 
-  get '/search' => 'products#search' do
-    member do
-      post :upvote  # /products/:id/upvote
-      # post :post_comment # /products/:id/post_comment
-    end
-  end
+  get '/search' => 'products#search'
 
 end
