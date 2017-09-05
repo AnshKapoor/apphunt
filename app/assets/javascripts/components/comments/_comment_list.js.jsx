6e5fabc4566667aeea2a8b8197e983359236970a
Charlie Jeppsson <charlie.jeppsson1@gmail.com>
2017-09-05 18:39:19 +0200

    order comments reverse and style comments section

--- app/assets/javascripts/components/comments/_comment_list.js.jsx
+++ app/assets/javascripts/components/comments/_comment_list.js.jsx
@@ -28,20 +28,31 @@ var CommentList = React.createClass({
   },
 
   render: function() {
+
+    var comments = [];
+    this.state.product.comments.map(function(comment){
+      if (comments.includes(comments) == false) {
+        comments.push(comment);
+      }
+    });
+    var comments = comments.reverse();
+
     return (
       <div className="comments-container">
-        <h3>Comments</h3>
-        {this.state.product.comments.map(function(comment){
-          return <Comment comment={comment} key={comment.id} />;
-        })}
+        {/* Post comment form */}
         <div>
           <form acceptCharset="UTF-8" className="comment-form">
-            <div className="form-group">
+            <div className="form-group post-comment-form">
               <textarea ref="content" name="content" type="text" placeholder="Write new comment here" className="comment-input form-text form-control"></textarea>
               <input className="btn post-comment-btn" onClick={this.postComment} type="submit" value="Post comment" />
             </div>
           </form>
         </div>
+        <h3>Comments</h3>
+        {/* List of comments */}
+        {comments.map(function(comment){
+          return <Comment comment={comment} key={comment.id} />;
+        })}
       </div>
     );
   },
