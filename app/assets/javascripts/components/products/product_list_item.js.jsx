d7e8e9f48de0b1f75ae907ae0ef82cb370e5b89b
Charlie Jeppsson <charlie.jeppsson1@gmail.com>
2017-09-07 16:29:53 +0200

    make product-list-item mobile responsive, only for xs

--- app/assets/javascripts/components/products/product_list_item.js.jsx
+++ app/assets/javascripts/components/products/product_list_item.js.jsx
@@ -14,43 +14,53 @@ var ProductListItem = React.createClass({
 
     return (
       <div>
-        <div className="product">
-          <div className="product-controls">
-            <Upvote product={this.props.product}/>
-
-            <div className="comment-controls" onClick={this.toggleCommentList}>
-              <div className="comments-button">
-                <i className="fa fa-commenting"></i>
-              </div>
-              <div className="comments-count">
-                {this.props.product.comments.length}
+        <div className="product container">
+          <div className="row product-comp-row">
+            <div className="product-body col-xs-10">
+              <h3>
+                <p><a href={this.props.product.url} target="_blank">{this.props.product.name}</a>{tagline}</p>
+              </h3>
+            </div>
+            <div className="product-avatar col-xs-2">
+              <div>
+                <Img src={this.props.product.user.avatar_url} className="product-badge"/>
               </div>
             </div>
           </div>
+          <div className="row product-comp-row">
+            <div className="col-xs-10">
+              <p className="product-description">{this.props.product.description}</p>
+            </div>
+            <div className="product-controls col-xs-2">
+              <Upvote product={this.props.product}/>
 
-          <div className="product-body">
-            <h3>
-              <p><a href={this.props.product.url} target="_blank">{this.props.product.name}</a>{tagline}</p>
-            </h3>
-            <p className="product-description">{this.props.product.description}</p>
-          </div>
-          <div className="product-badges">
-            {this.props.product.for_sale ? (
-              <div className="product-badge for-sale-badge hidden-xs hidden-sm">
-                <p>For sale</p>
+              <div className="comment-controls" onClick={this.toggleCommentList}>
+                <div className="comments-button">
+                  <i className="fa fa-commenting"></i>
+                </div>
+                <div className="comments-count">
+                  {this.props.product.comments.length}
+                </div>
               </div>
-            ) : (
-              <div className="product-badge wanted-badge">
-                <p>Wanted</p>
-              </div>
-            )}
-            <div>
-              <Img src={this.props.product.user.avatar_url} className="product-badge"/>
+            </div>
+
+            <div className="hidden-xs">
+              {this.props.product.for_sale ? (
+                <div className="product-badge for-sale-badge hidden-xs hidden-sm">
+                  <p>For sale</p>
+                </div>
+              ) : (
+                <div className="product-badge wanted-badge">
+                  <p>Wanted</p>
+                </div>
+              )}
             </div>
           </div>
-        </div>
-        <div>
-          { this.state.showCommentList ? <CommentList product={this.props.product} /> : null }
+          <div className="row product-comp-row">
+            <div className="col-xs-12">
+              { this.state.showCommentList ? <CommentList product={this.props.product} /> : null }
+            </div>
+          </div>
         </div>
       </div>
     );
